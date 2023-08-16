daf3d5cfef0fc0ad9bb3fa34ebdfb6e0ef803b81
Talha Maqsood <68240954+talhaa99@users.noreply.github.com>
2023-08-16 16:35:19 +0500

    debug

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.route('/_api/package.json')
   
 app.route('/')
     .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
+		  res.sendFile(__dirname + '/views/index.html');
     })
 
 // Respond not found to all the wrong routes
